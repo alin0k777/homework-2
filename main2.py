1624704733938a9a784b8b8afdaf78e2cedba5d5
Alin0k <matvieiukalina@gmail.com>
2024-06-13 13:29:56 +0300

    Fix for homework 2.2

--- main2.py
+++ main2.py
@@ -1,8 +1,8 @@
 print("please write me a five digit number")
 number = input()
-print(int(number) % 10)
-print(int(number) // 10 % 10)
-print(int(number) // 100 % 10)
-print(int(number) // 1000 % 10)
-print(int(number) // 10000 % 10)
+print(int(number) % 10,
+      int(number) // 10 % 10,
+      int(number) // 100 % 10,
+      (int(number) // 1000 % 10),
+      (int(number) // 10000 % 10), sep='')
 
