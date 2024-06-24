84482fc0d82222176e667901526530861eeaec85
Alin0k <matvieiukalina@gmail.com>
2024-06-24 22:07:39 +0300

    homework 5.2

---
+++ main5.2.py
@@ -0,0 +1,24 @@
+while True:
+    first = int(input("Введите первое число: "))
+    second = int(input("Введите второе число: "))
+    what = input("Выберите действие, могу предложить: +, -, *, /:")
+
+    if what == "+":
+        print("Результат:", first + second)
+    elif what == "-":
+        print("Результат:", first - second)
+    elif what == "*":
+        print("Результат:", first * second)
+    elif what == "/":
+        if second != 0:
+            print("Результат:", first / second)
+        else:
+            print("Делить на ноль нельзя")
+    else:
+        print("Неверное действие")
+
+    continue_calculating = input("Хотите продолжить? (yes/no для продолжения): ")
+    if continue_calculating.lower() not in ["yes", "y"]:
+        break
+
+print("Работа калькулятора завершена")
