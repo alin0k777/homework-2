f2fa16f3b4818c7a0bd2575bb8c6b709a5e06720
Alin0k <matvieiukalina@gmail.com>
2024-07-04 00:16:58 +0300

    homework 5.1

---
+++ main5.1.py
@@ -0,0 +1,27 @@
+import keyword
+import string
+
+
+def is_valid_variable_name(word):
+
+    if word[0].isdigit():
+        return False
+
+    for el in word:
+        if el.isupper():
+            return False
+
+        if el in string.punctuation.replace('_', ''):
+            return False
+
+    if word in keyword.kwlist:
+        return False
+
+    if word.count('_') > 1:
+        return False
+
+    return True
+
+
+user_input = input("Enter word: ")
+print(is_valid_variable_name(user_input))
